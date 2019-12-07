da981b4dbaa1e004461885f0add5d85ab6f5adb2
anishmathew19 <58158241+anishmathew19@users.noreply.github.com>
2019-12-07 14:55:48 +0530

    Static variable example

---
+++ StaticExample.java
@@ -0,0 +1,17 @@
+package corepgm;
+
+public class StaticExample {
+	static int count=0;
+	StaticExample()
+	{
+		count++;
+		System.out.println(count);
+	}
+	public static void main(String args[])
+	{
+		StaticExample c1=new StaticExample();
+		StaticExample c2=new StaticExample();
+		StaticExample c3=new StaticExample();
+	}
+
+}
