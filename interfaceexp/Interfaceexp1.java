32f90e4c8db7d91d0cfde81a02e5c438fd72ae07
anishmathew19 <58158241+anishmathew19@users.noreply.github.com>
2019-12-11 10:55:34 +0530

    interface example

---
+++ interfaceexp/Interfaceexp1.java
@@ -0,0 +1,7 @@
+package interfaceexp;
+
+public interface Interfaceexp1 {
+	void add(int a,int b);
+	void sub(int a,int b);
+
+}
---
+++ interfaceexp/Interfaceexp2.java
@@ -0,0 +1,7 @@
+package interfaceexp;
+
+public interface Interfaceexp2 {
+	void mult(int a,int b);
+	void div(int a,int b);
+
+}
