e5fac7bf7b310b7e5f054858125e045ebe46bdce
anishmathew19 <58158241+anishmathew19@users.noreply.github.com>
2019-12-12 12:50:06 +0530

    Customer  registration
    Customer registration using Constructor

---
+++ CustomerRegistration.java
@@ -0,0 +1,44 @@
+package corepgm;
+
+import java.util.Scanner;
+
+public class CustomerRegistration 
+{
+	String name;
+	String address;
+	String email;
+	int phno;
+	
+	public CustomerRegistration(String name,String address,String email,int phno)
+	{
+		this.name=name;
+		this.address=address;
+		this.email=email;
+		this.phno=phno;
+	}
+	public void display() 
+	{
+		System.out.println("\n***User Details***");	
+		System.out.println("Name:"+name);
+		System.out.println("Address:"+address);
+		System.out.println("Email:"+email);
+		System.out.println("Contact Number:"+phno);
+	}
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter User Details");	
+		System.out.println("Enter  user name");	
+		String name=s.next();
+		System.out.println("Enter address");	
+		String address=s.next();
+		System.out.println("Enter email");	
+		String email=s.next();
+		System.out.println("Enter Phone Number");	
+		int phno=s.nextInt();
+		CustomerRegistration obj=CustomerRegistration(name,address,email,phno);
+		obj.display();
+				  
+
+}
+}
